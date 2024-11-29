e907c8c283c302da1d06668696ddc8ef4ffe2aa5
DesiZafirova <desszafirowa@gmail.com>
2024-11-29 18:02:37 +0200

    creating redux slices and reorganizing the code structure

---
+++ src/features/accounts/accountSlice.js
@@ -0,0 +1,59 @@
+const initialStateAccount = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: '',
+};
+
+export default function accountReducer(state = initialStateAccount, action) {
+  switch (action.type) {
+    case 'account/deposit':
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+      };
+    case 'account/withdraw':
+      return {
+        ...state,
+        balance: state.balance - action.payload,
+      };
+    case 'account/requestLoan':
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        balance: state.balance + action.payload.amount,
+        loanPurpose: action.payload.purpose,
+      };
+    case 'account/payLoan':
+      if (state.balance < state.loan) return state;
+      return {
+        ...state,
+        balance: state.balance - store.loan,
+        loan: 0,
+        loanPurpose: '',
+      };
+    default:
+      return state;
+  }
+}
+
+export function deposit(amount) {
+  return { type: 'account/deposit', payload: amount };
+}
+
+export function withdraw(amount) {
+  return { type: 'account/withdraw', payload: amount };
+}
+
+export function requestLoan(amount, purpose) {
+  return {
+    type: 'account/requestLoan',
+    payload: { amount: amount, purpose: purpose },
+  };
+}
+export function payLoan() {
+  return { type: 'account/payLoan' };
+}
+
+// store.dispatch(requestLoan(1000, 'to buy a car'));
+// store.dispatch(payLoan());
